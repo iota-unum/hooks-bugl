6a216aa51cc1f4430c761bae41d0cadf116ba0f2
luigi <luigilamastra@gmail.com>
2020-06-09 05:06:33 +0200

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="flex h-screen">
+
+      <div className="flex bg-indigo-500 text-white w-2/3 h-64 m-auto">
+        <p className='m-auto text-5xl'>
+
+
+        React + Tailwind
+        </p>
+        
+        
+        </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: { backgroundColor: {
+      'green-t-50': 'rgba(165, 55, 253, 0.5)',
+  },},
+  },
+  variants: {},
+  plugins: [],
+}
